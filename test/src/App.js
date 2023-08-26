d81242889c8ce795b5644fd3c5e4070efa3bbd13
Adedoyinlorukomi <akindoyin800@gmail.com>
2023-08-26 19:45:31 +0100

    commit

--- test/src/App.js
+++ test/src/App.js
@@ -23,7 +23,30 @@ export default function App() {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
 
+// import logo from './logo.svg';
+// import React from 'react';
+// import './App.css';
+// import Layout from "./Pages/Layout";
+// import Home from './Pages/Home'
+// import Dashboard from './Pages/Dashboard';
+// import ReactDOM from "react-dom/client";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+// function App() {
+//   return (
+//       <Router>
+//         <Routes>
+//         <Route path="/" exact component={Layout}/>
+//         <Route path='/Home' Component={Home} />
+//         <Route path='/Dashboard' component={Dashboard} />
+//         </Routes>
+//       </Router>
+//   );
+// }
+
+// // const root = ReactDOM.createRoot(document.getElementById('root'));
+// // root.render(<App />);
+// export default App;
 
 
  
